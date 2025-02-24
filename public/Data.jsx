949a7d69eacee45ef5058baa7ebb7e0aa9ae19f8
Lanrecode23 <lanrecode23@gmail.com>
2025-02-24 13:11:15 +0100

    commited

--- public/Data.jsx
+++ public/Data.jsx
@@ -26,8 +26,18 @@ const Data = [
         github:"https://github.com/Lanrecode23/msoftgemin",
         techStack: ['HTML', ' CSS', ' JavaScript', ' API']
     },
+        {
+            "id": 4,
+            "title": "LagosSchoolsOnline",
+            "img": "/img/lagos.png",
+            "details": "An internet community for all schools in the public and private sectors in Lagos State. I worked on the project by implementing HTML, CSS, Bootstrap, and a touch of JavaScript to enhance responsiveness and interactivity.",
+            "link": "https://lagosschoolsonline.com/",
+            "github": "https://github.com/Lanrecode23/",
+            "techStack": ["HTML", "CSS", "Bootstrap", "JavaScript"]    
+    },
     {
-        id: 4,
+        
+        id: 5,
         title: 'Snap Template',
         img: '/img/snap.png',
         details: 'This site was built using HTML, CSS, and Bootstrap. This project has broadened my knowledge of Bootstrap, helping me create a simplified template using it. I found this project on the Frontend Mentor site and decided to take it on.',
@@ -36,7 +46,7 @@ const Data = [
         techStack: ['HTML', ' CSS', ' Bootstrap']
     },
     {
-        id: 5,
+        id: 6,
         title: 'Todo List App',
         img: '/img/todo.png',
         details: 'This is a Todo List app built using React.js. The site allows you to keep track of your tasks using the useContext API, and you can also delete tasks that are completed. It incorporates state management for adding and deleting tasks through the useContext API.',
@@ -45,7 +55,7 @@ const Data = [
         techStack: ['React.js', ' useContext API']
     },
     {
-        id: 6,
+        id: 7,
         title: 'Memory Game',
         img: '/img/memory.png',
         details: 'This website was built using React.js and Vite. It’s a memory game where the goal is to match pairs of items. I used a custom hook to encapsulate reusable logic, along with the Context API to avoid prop drilling and make the code more simplified and manageable.',
@@ -54,7 +64,7 @@ const Data = [
         techStack: ['React.js', ' Vite', ' Custom Hook', ' Context API']
     },
     {
-        id: 7,
+        id: 8,
         title: 'E-Commerce Site',
         img: '/img/ecommerce.png',
         details: 'This is an e-commerce site built using React.js, along with the Redux library for state management. Features like adding items to the cart, increasing the cart count, and deleting items from the cart are handled using Redux. The items in the cart are also stored in localStorage to ensure persistence, allowing the cart items to be retained even after refreshing the page.',
@@ -63,7 +73,7 @@ const Data = [
         techStack: ['React.js', ' Redux', ' localStorage']
     },
     {
-        id: 8,
+        id: 9,
         title: 'Financial Tracker',
         img: '/img/fintrack.png',
         details: 'This is a financial tracker built using React, Vite, and Firebase as the backend. The app allows you to track your expenses, income, and balance, giving you insights into your spending. Firebase handles authentication for sign-in and sign-up, while Firestore is used to store your expense data. This project has provided a solid foundation for learning Firebase.',
